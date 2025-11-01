e5d0c264634d0180377cc5f4b77dc030a186774b
eto4detak <niclokitor@gmail.com>
2025-11-01 21:02:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Heaader";
+import Image from "./components/Image";
+import logo from "./img/logo.png"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    helpText = "Help Text!";
+
+    inputClick()  {console.log("input click")} 
+    inputEnter()  {console.log("input Enter")} 
+
+    render(){
+        return (
+            <div className="App">
+                <Header title="Шапка Сайта" />
+                <header className="App-header">
+                    
+                    <h1>{this.helpText}</h1>
+                    <input placeholder={this.helpText} onClick={this.inputClick} onMouseEnter={this.inputEnter} /> <br />
+                    <p>{this.helpText === "Help Text!" ? "Yes" : "No"}</p>
+                    <Image image={logo} />
+                    <img alt="" src={logo} />
+                </header>
+            </div>
+        );
+    }
+
+    
+
 }
 
 export default App;
